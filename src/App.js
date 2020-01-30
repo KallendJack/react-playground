893db3dc67af6260ebeda28a5528acbde3f7cfbc
Jack Kallend <Kallendo96@gmail.com>
2020-01-30 15:14:11 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="text-xl tracking-widest">Hello World!</h1>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="text-xl tracking-widest md:text-blue-400">
+          Hello World!
+        </h1>
+      </div>
+    )
+  }
 }
 
 export default App

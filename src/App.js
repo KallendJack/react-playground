d67ce9ca98b16f1a1c203faeabb700383fe8768a
Jack Kallend <Kallendo96@gmail.com>
2020-01-30 14:51:25 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-      <button className="px-2 py-1 rounded-lg bg-green-400 text-green-800 text-xl font-light uppercase shadow-md hover:shadow-lg">
-        Hello World!
-      </button>
+      <h1 className="text-xl tracking-widest">Hello World!</h1>
     </div>
   )
 }

ba4850d55d666a3850aa65d9ee47ca74c9c475c5
Jack Kallend <Kallendo96@gmail.com>
2020-01-30 16:28:39 +0000

    Context tailwind

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -6,7 +6,19 @@ class Index extends Component {
     return (
       <Consumer>
         {value => {
-          return value.contacts.map(contact => contact.name)
+          return value.contacts.map(contact => (
+            <div
+              key={contact.id}
+              className="flex items-center justify-center w-full py-8"
+            >
+              <div className="overflow-hidden rounded max-w-xs w-full shadow-lg leading-normal">
+                <div className="block group p-4">
+                  <p className="font-bold text-lg mb-1">{contact.name}</p>
+                  <p className="mb-2">{contact.email}</p>
+                </div>
+              </div>
+            </div>
+          ))
         }}
       </Consumer>
     )

5bb2fe35dbd834c03067a0ce6cbb324bc2db6626
Jack Kallend <Kallendo96@gmail.com>
2020-02-05 11:29:07 +0000

    Update View.js

--- src/pages/View.js
+++ src/pages/View.js
@@ -8,7 +8,7 @@ class View extends Component {
       <Consumer>
         {({ posts }) => {
           const post = posts.filter(
-            post => post.id == this.props.match.params.id
+            post => post.id === this.props.match.params.id
           )
 
           return <h1>{post.id}</h1>
